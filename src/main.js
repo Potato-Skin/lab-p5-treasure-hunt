2580c4a805c644845382fd42d8bc1b26b70bc492
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-02-11 22:17:13 +0100

    Finally got on the grid

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const game = new Game();
 
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
-  canvas.parent("canvas");
+  createCanvas(WIDTH, HEIGHT);
 }
 
 function draw() {
+  background(220)
   game.drawGrid();
 }
